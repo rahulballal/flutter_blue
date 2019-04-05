3fe3c90bc9a9a06ac0f7192898e3568c6b1598eb
Rahul Ballal <rahulballal@gmail.com>
2019-04-05 11:05:27 +1100

    patch compilation issue

--- example/lib/widget_characteristic_tile.dart
+++ example/lib/widget_characteristic_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue_example/widget_descriptor_tile.dart';
 
 class CharacteristicTile extends StatelessWidget {
   final BluetoothCharacteristic characteristic;
